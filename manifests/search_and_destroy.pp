66a2d619978856ae64541a3ca5a334dbde33e643
steroidg <steroidg@gmail.com>
2017-07-19 23:22:23 +1000

    got basic functionality in place

--- manifests/search_and_destroy.pp
+++ manifests/search_and_destroy.pp
@@ -1,12 +1,25 @@
-#
 # This is an example of how to document a defined type.
+# @param filename Name of the file to search string
 # @param search_str Search string
 # @param notify_only Display notification only if found
 
 define snd::search_and_destroy (
+  String  $file_name = $name,
   String  $search_str,
   Boolean $display_only = true,
 ) {
-  notify { "$name search_str = $search_str": }
-  notify { "$name display_only = $display_only": }
+  if $::snd::params::display_only == true {
+    $snd_cmd = "echo ${filename}"
+  } else {
+    $snd_cmd = "rm ${file_name}"
+  }
+
+  # Remove file if duplicate found.
+  exec {
+    "snd ${filename}":
+      command => $snd_cmd,
+      path    => '/bin:/sbin:/usr/bin:/usr/sbin',
+      onlyif  => "[ -s ${file_name} ] && sed s/\<*.duid\>//g ${file_name} | grep ${duid}",
+  }
+
 }
