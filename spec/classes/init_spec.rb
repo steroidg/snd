1f2fa91c9d803fc239e6a2f351b065c41ed53ec8
steroidg <steroidg@gmail.com>
2017-08-04 23:01:58 +1000

    Issue #2 got a more elaborate spec test for init.pp

--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -1,6 +1,33 @@
 require 'spec_helper'
+
 describe 'snd' do
+  let(:title) { 'snd' }
+
   context 'with default values for all parameters' do
+  	it { is_expected.to compile }
+		it { is_expected.to compile.with_all_deps }
     it { should contain_class('snd') }
+  end
+
+	context 'with wrong variable type' do
+		let(:params) { {
+		  :search_items => 'some string',	
+    } }
+    it { should compile.and_raise_error(/expects a Hash value, got String/) }
+	end
+
+  context 'with valid param' do
+    let(:params) {
+      {
+        'search_items' => {
+				  '/tmp/foo' => {
+					  'search_str'   => '^then_im_sorry$',
+            'display_only' => true
+          }
+        }
+      }
+    }
+  	  it { is_expected.to compile }
+		  it { is_expected.to compile.with_all_deps }
   end
 end
